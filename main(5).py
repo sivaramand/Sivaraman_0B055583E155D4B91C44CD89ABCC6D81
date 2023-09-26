8a1bc0da30ed6cf4c5afe0922c89493ce5925771
sivaramand <144401699+sivaramand@users.noreply.github.com>
2023-09-26 21:48:32 +0530

    Add files via upload

---
+++ main(5).py
@@ -0,0 +1,12 @@
+"""write a program that determines whether the year is leap year or not"""
+def isleapYear(year):
+ if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+  return True
+ else:
+  return False
+
+year= int(input("Enter a year :"))
+if isleapYear(year):
+ print(" {} is leap year.". format(year))
+else:
+ print(" {} is not leap year.". format(year))
